4cbcc4643b0e177f71ea0e2e3de6029417cab5f3
merghavikavin <merghavikavin99@gmail.com>
2022-02-26 13:07:19 +0530

    cmd

---
+++ 3_Implementation/test/test.c
@@ -0,0 +1,25 @@
+#include "unity.h"
+#include "func.h"
+
+void setUp()
+{
+
+}
+void tearDown()
+{
+  
+}
+void test_procuct billing()
+{
+    int c=2;
+    TEST_ASSERT_EQUAL(1,product billing(c));
+    
+}
+
+int main()
+{
+  UNITY_BEGIN();
+  
+  RUN_TEST(test_product billiing;
+  return UNITY_END();
+}
